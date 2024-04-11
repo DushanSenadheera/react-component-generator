b170b27193d08f03763318c7f45317df2170a7a5
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-04-11 20:24:30 +0530

    modify component create path

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ const createComponent = require('./createComponent');
 // Get the component name from the command line arguments
 let componentName = process.argv[2];
 if (!componentName) {
-    console.error('Please provide a component name');
+    console.error('❌ Please provide a component name');
     process.exit(1);
 }
 
 // Validate the component name
 if (!validate(componentName)) {
-    console.error('Invalid component name');
+    console.error('❌ Invalid component name');
     process.exit(1);
 }
 
-const dir = path.join('components', componentName);
+// Prepend 'src/' to the dir variable
+const dir = path.join('src', 'components', componentName);
 
 // Create the component
 createComponent(dir, componentName);
